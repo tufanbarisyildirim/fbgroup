0b0297313fece40c9d4ebab6aa50cad7ea56ec1d
Tufan Barış YILDIRIM <tufanbarisyildirim@gmail.com>
2013-10-31 21:45:17 +0200

    default controller to dashboard

--- application/config/routes.php
+++ application/config/routes.php
@@ -73,7 +73,7 @@
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'dashboard';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
