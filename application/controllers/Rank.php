2b69a2ba1fd0a59377ba301396aa783ffba99fa7
Tufan Barış YILDIRIM <tufanbarisyildirim@gmail.com>
2013-11-19 01:32:54 +0200

    top 5 chart prototype is ready

--- application/controllers/Rank.php
+++ application/controllers/Rank.php
@@ -9,8 +9,22 @@
         public function top5s()
         {                                           
             //load all tracks including all quizzes!
-            $data['panels'] = array(); //horrays
+            $panels = array(); //horrays   
 
+            $quizzes = $this->db->query("SELECT * FROM quizzes")->result_array();
+
+            foreach($quizzes as $quiz)
+            {
+                if(!isset($panels[$quiz['track_id']]))
+                    $panels[$quiz['track_id']] = array();
+                   
+                 $quiz['top5'] = array();
+                 $quiz['top5']  =   $this->db->query("SELECT qs.*,u.* FROM quiz_scores qs JOIN users u ON u.id = qs.user_id AND qs.quiz_id =  " . $quiz['id']." ORDER BY qs.score DESC LIMIT 5")->result_array();
+                    
+                $panels[$quiz['track_id']][] = $quiz;
+            }    
+            
+            $data['panels'] = $panels;
             $this->load->view('rank/top5s',$this->common_data($data));
         }
     }
--- application/views/rank/top5s.php
+++ application/views/rank/top5s.php
@@ -26,16 +26,53 @@
             </li>
         </ol>
         <div class="page-header">
-            <h1>Questions <small>ask a new one</small></h1>
+            <h1>Top 5s <small>these charts will show you as soon as you believed!</small></h1>
         </div>
         <!-- end: PAGE TITLE & BREADCRUMB -->
     </div>
 </div>
 
-<div class="row">
-    <div class="col-md-12">
-    track panel including quizzes order by track_id desc, quiz_date desc! limit 5  for each quiz!
-    </div>
+<?php foreach($panels as $track_id => $panel):?>
+
+    <div class="row">
+        <div class="col-md-12">
+
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <i class="icon-question-sign"></i>
+                    Track # <?php echo $track_id; ?>
+                </div>
+                <div class="panel-body">  
+
+                    <?php foreach($panel as $quiz):?>    
+                        <div class="col-md-6">
+
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <i class="icon-question-sign"></i>
+                                   <?php echo $quiz['name']; ?>
+                                </div>
+                                <div class="panel-body">
+                                    <table class="table table-bordered table-condensed">
+                                        <tr><th>Name</th><th>Score</th></tr>
+                                        <?php foreach($quiz['top5'] as $user): ?>
+                                            <tr><td><img src="<?php echo fb_profile_pic_url($user['user_id']); ?>" class="circle-img" width="30" />&nbsp;<?php echo $user['name']; ?> <?php echo $user['surname']; ?></td><td><?php echo $user['score']; ?></td></tr>
+                                            <?php endforeach;?>
+                                    </table>
+                                </div>
+                            </div> 
+
+                        </div>
+
+                        <?php endforeach;?>
+
+
+                </div>
+            </div> 
+
         </div>
+    </div>   
+    <?php endforeach;?>
+    
         
 <?php echo get_instance()->footer(); ?>
