d77b1171673d5ae18e866858f0f39a915ef97b09
Tufan Barış YILDIRIM <tufanbarisyildirim@gmail.com>
2014-01-08 20:51:28 +0200

    google analytics code.

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -36,24 +36,16 @@
 		</div>
 	</div>
 </div>
-<!-- xmas_lights [ start ] -->
-<!--script type="text/javascript">
-	document.write("<div id='lights' style='position:absolute;width:956px;height:180px;top:-20px;background:url(<?php echo assets_url() . '/images/'; ?>light.png) no-repeat;left:50%;margin-left:-478px'></div>")
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	setInterval( "xmasLights();", 1000 );
-	var lights = "active";
-	function xmasLights(){
-		var img = document.getElementById('lights');
-		if(lights == "active"){
-			img.style.backgroundPosition = "0 -180px";
-			lights = "inactive";
-		}else{
-			img.style.backgroundPosition = "";
-			lights = "active";
-		}
-	} 
+	ga('create', 'UA-19794840-11', 'prepeng.com');
+	ga('send', 'pageview');
+
 </script>
-<!-- xmas_lights [ end ] -->
     </body>
     <!-- end: BODY -->
 </html>
